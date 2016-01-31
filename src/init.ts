12bd1a9f3a9c904c0658f8d382dc606ceb9bb1f6
Homa Wong <homawong@gmail.com>
2016-01-31 01:36:38 -0800

    Implement init
    Also update repo and license in package.json

--- src/init.ts
+++ src/init.ts
@@ -1,4 +1,22 @@
 import * as vscode from 'vscode';
+import * as childProcess from 'child_process';
+import 'bluebird';
+
+function initFn(rootPath): Promise<any> {
+  return new Promise((resolve, reject) => {
+    childProcess.exec('typings init',
+      { cwd: rootPath },
+      (error, stdout, stderr) => {
+        if (stderr) {
+          reject(new Error('typings.json already exists'));
+        }
+        else {
+          resolve();
+        }
+      }
+    )
+  });
+}
 
 export var initCommand = {
   name: 'typings.init',
@@ -7,6 +25,11 @@ export var initCommand = {
       vscode.window.showInformationMessage('No folder opened');
       return;
     }
-    vscode.window.showInformationMessage('Will be ready real soon!');
+
+    initFn(vscode.workspace.rootPath).then(() => {
+        vscode.window.showInformationMessage('typings.json created');
+    }).catch((err)=> {
+        vscode.window.showErrorMessage('typings.json already exists');
+    });
   }
 };
